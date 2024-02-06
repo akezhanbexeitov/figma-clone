b1f48a7e43657b5768946b866e892b7d9d98a2f3
Akezhan <akidulatini@gmail.com>
2024-02-06 19:39:57 +0600

    Create live cursors functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+
+import Live from "@/components/Live";
+
 export default function Page() {
   return (
-    <div className='flex h-[100vh] w-full items-center justify-center text-center'>
-      <h1 className='text-2xl text-white'>Hello world</h1>;
+    <div>
+      <Live />
     </div>
   );
 }
