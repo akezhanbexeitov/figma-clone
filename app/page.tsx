cfbd2edc7a89630b8b36a9459bafea36fb57ad50
Akezhan <akidulatini@gmail.com>
2024-02-06 16:30:38 +0600

    Init

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-  return <h1 className='text-5xl text-white'>Hello world</h1>;
+  return (
+    <div className='flex h-[100vh] w-full items-center justify-center text-center'>
+      <h1 className='text-2xl text-white'>Hello world</h1>;
+    </div>
+  );
 }
